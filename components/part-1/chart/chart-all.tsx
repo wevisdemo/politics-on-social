37696394cc289e82d7fd1824f7307940063f89d7
bariyard <worawitrr@gmail.com>
2022-10-24 14:21:10 +0700

    minor fix

--- components/part-1/chart/chart-all.tsx
+++ components/part-1/chart/chart-all.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { HeadDecorationLeft, HeadDecorationRight } from '../../utils'
 import { GOVERNMENT, OPPOSITION } from './chart-data'
 
 type Props = {}
@@ -49,17 +50,15 @@ const ChartAll = ({ type }: { type: "all" | "facebook" | "twitter" | "youtube" }
   }
 
   return (
-    <div className='flex flex-row
+    <div className='flex flex-row justify-center
     divide-x divide-dashed'>
-      <div className='pr-[15px]'>
-        <div className='flex flex-row items-center justify-between opacity-60'>
-          <svg className='w-[28px] h-[6px]' viewBox="0 0 34 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M0.333333 3C0.333333 4.47276 1.52724 5.66667 3 5.66667C4.47276 5.66667 5.66667 4.47276 5.66667 3C5.66667 1.52724 4.47276 0.333333 3 0.333333C1.52724 0.333333 0.333333 1.52724 0.333333 3ZM3 3.5H34V2.5H3V3.5Z" fill="white" />
-          </svg>
-          <div className='wv-font-anuphan wv-font-bold text-desktop-bold-b6'>ฝ่ายรัฐบาล</div>
-          <svg className='w-[28px] h-[6px]' viewBox="0 0 34 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M28.3333 3C28.3333 4.47276 29.5272 5.66667 31 5.66667C32.4728 5.66667 33.6667 4.47276 33.6667 3C33.6667 1.52724 32.4728 0.333333 31 0.333333C29.5272 0.333333 28.3333 1.52724 28.3333 3ZM0 3.5H31V2.5H0V3.5Z" fill="white" />
-          </svg>
+      <div className="pr-[20px]">
+        <div className="flex flex-row gap-x-[5px] opacity-60">
+          <HeadDecorationLeft />
+          <div className="flex-none wv-font-anuphan wv-font-bold text-mobile-bold-b6 ">
+            ฝ่ายรัฐบาล
+          </div>
+          <HeadDecorationRight />
         </div>
         {GOVERNMENT.map((data) => (
           <div key={`gov-${data.name}`} className='text-mobile-bold-b7 mt-[34px]'>
@@ -85,15 +84,13 @@ const ChartAll = ({ type }: { type: "all" | "facebook" | "twitter" | "youtube" }
           </div>
         ))}
       </div>
-      <div className='pl-[15px]'>
-        <div className='flex flex-row items-center justify-between opacity-60'>
-          <svg width={34} height={6} viewBox="0 0 34 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M0.333333 3C0.333333 4.47276 1.52724 5.66667 3 5.66667C4.47276 5.66667 5.66667 4.47276 5.66667 3C5.66667 1.52724 4.47276 0.333333 3 0.333333C1.52724 0.333333 0.333333 1.52724 0.333333 3ZM3 3.5H34V2.5H3V3.5Z" fill="white" />
-          </svg>
-          <div className='wv-font-anuphan wv-font-bold text-desktop-bold-b6'>ฝ่ายค้าน</div>
-          <svg width={34} height={6} viewBox="0 0 34 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M28.3333 3C28.3333 4.47276 29.5272 5.66667 31 5.66667C32.4728 5.66667 33.6667 4.47276 33.6667 3C33.6667 1.52724 32.4728 0.333333 31 0.333333C29.5272 0.333333 28.3333 1.52724 28.3333 3ZM0 3.5H31V2.5H0V3.5Z" fill="white" />
-          </svg>
+      <div className="pl-[20px]">
+        <div className="flex flex-row gap-x-[5px] opacity-60">
+          <HeadDecorationLeft />
+          <div className="flex-none wv-font-anuphan wv-font-bold text-mobile-bold-b6 ">
+            ฝ่ายค้าน
+          </div>
+          <HeadDecorationRight />
         </div>
         {OPPOSITION.map((data) => (
           <div key={`opp-${data.name}`} className='text-mobile-bold-b7 mt-[34px]'>
