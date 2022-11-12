54ca0324f64f74d6a1a0413d744c6e9d7912375e
Th1nkK1D <witheep@gmail.com>
2022-11-12 02:21:15 +0200

    feat: Add cookie consent and clarity snippet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 import '@wevisdemo/ui/styles/index.css';
+import WvCookieConsent from '@wevisdemo/ui/react/cookie-consent';
+import { initClarity } from '../utils/clarity';
+
+function onCookieAccept(options: Record<string, boolean>) {
+  if (options['Analytics']) {
+    initClarity();
+  }
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-    </>)
+      <WvCookieConsent
+        policyUrl='https://punchup.world'
+        cookieOptions={['Analytics']}
+        onAccept={onCookieAccept}
+      />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
