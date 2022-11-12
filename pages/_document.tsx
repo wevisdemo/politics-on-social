cfe54910e100a8451e14860d8986bf2d44379571
Th1nkK1D <witheep@gmail.com>
2022-11-12 02:31:03 +0200

    chore: Remove unused typo link stylesheet

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,20 +1,12 @@
-
-import { Html, Head, Main, NextScript } from 'next/document'
-
+import { Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://design-systems.wevis.info/typography.css"
-        />
-      </Head>
       <body className='scroll-smooth'>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
