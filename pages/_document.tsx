9575d143e823c52165e8b39ca2f6e7c4c4073734
Th1nkK1D <witheep@gmail.com>
2022-11-15 12:01:09 +0200

    chore: Add Plausible

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,14 @@ import { Html, Main, Head, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html>
-      <Head />
-      <body className='scroll-smooth'>
+      <Head>
+        <script
+          defer
+          data-domain="wevis.info/politics-on-social"
+          src="https://analytics.punchup.world/js/plausible.js"
+        ></script>
+      </Head>
+      <body className="scroll-smooth">
         <Main />
         <NextScript />
       </body>
